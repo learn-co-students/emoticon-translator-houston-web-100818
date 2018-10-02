935d45509653dd5559b182895d24d8ed775b3ab4
no-relation <eddiec76@gmail.com>
2018-10-02 10:27:43 -0700

    lab complete

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,46 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  library = {}
+  library["get_meaning"] = {}
+  library["get_emoticon"] = {}
+#library = {"get_meaning" => {jemoticon => word}, "get_emoticon" => {engmoticon => word}}
+
+  emoticons.each do |word,symbols|
+    library["get_meaning"][symbols[1]] = word
+    library["get_emoticon"][symbols[0]] = symbols[1]
+  end
+  
+  library
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  library = load_library(file)
+
+  if library["get_emoticon"].key?(emoticon)
+    library["get_emoticon"].each do |engmoticon, nipponicon|
+      if engmoticon == emoticon
+        return nipponicon
+      end
+    end
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  library = load_library(file)
+
+  if library["get_meaning"].key?(emoticon)
+    library["get_meaning"].each do |nipponicon, meaning|
+      if nipponicon == emoticon
+        return meaning
+      end
+    end
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
